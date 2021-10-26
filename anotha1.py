2bd41d016749b4e4fe0fa031d2623bd3d2270bf2
Septafaustino <septafaustino17@gmail.com>
2021-10-26 20:24:23 +0700

    Error test

--- anotha1.py
+++ anotha1.py
@@ -1,7 +1,9 @@
-from main import AdditionTest
+from main import AdditionTest, MultiplicationTest
 
 def TestAdd():
         assert AdditionTest(2,3) == 5
+        assert AdditionTest(5,3) == 8
+        assert MultiplicationTest(2,2) == 10
         print("Add Function works correctly")
 
 if __name__ == '__main__':
