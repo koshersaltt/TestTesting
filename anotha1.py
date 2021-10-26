3a1228e106ab10fecbaca33a669544a2615eddb6
Septafaustino <septafaustino17@gmail.com>
2021-10-26 20:26:00 +0700

    Fixed the issue on iputtest

--- anotha1.py
+++ anotha1.py
@@ -3,7 +3,6 @@ from main import AdditionTest, MultiplicationTest
 def TestAdd():
         assert AdditionTest(2,3) == 5
         assert AdditionTest(5,3) == 8
-        assert MultiplicationTest(2,2) == 10
         print("Add Function works correctly")
 
 if __name__ == '__main__':
