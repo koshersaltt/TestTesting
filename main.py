3e075817e5d4acf39ab25750b4195b728c71fc94
Septafaustino <septafaustino17@gmail.com>
2021-10-26 20:21:00 +0700

    Test

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def AdditionTest(a, b):
         return a + b
-        
+
+def MultiplicationTest(c,d):
+        return c * d
+
 def SayHello():
         print("Messing up with automated testing")
 
